6d394ad05d8532a3ba310ed2158d9c5f707963ac
Jan Olkowski <jan.olkowski@gmail.com>
2024-11-14 11:38:04 -0500

    Initial commit.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
 use tokio::net::TcpListener;
+use zero_to_prod::configuration::get_configuration;
+use zero_to_prod::{get_subscriber, init_subscriber, run};
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    zero_to_prod::run(listener).await;
+    init_subscriber(get_subscriber(
+        "zero-to-prod".into(),
+        "debug".into(),
+        std::io::stdout,
+    ));
+
+    
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to Postgres.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address).await.unwrap();
+    run(listener, connection_pool).await;
 }
